5bae65974130b2d454eaf4ac91951a385b7e7dff
Acrylic125 <acrylic125email@gmail.com>
2024-12-04 15:15:37 +0800

    day 4

--- 4/b.cpp
+++ 4/b.cpp
@@ -46,42 +46,92 @@ vector<char> word{
     'X', 'M', 'A', 'S'
 };
 
+
+vector<vector<vector<char>>> MAS_MATRICES{
+    {
+        { 'M', '.', 'S' },
+        { '.', 'A', '.' },
+        { 'M', '.', 'S' },
+    },
+    {
+        { 'S', '.', 'M' },
+        { '.', 'A', '.' },
+        { 'S', '.', 'M' },
+    },
+    {
+        { 'S', '.', 'S' },
+        { '.', 'A', '.' },
+        { 'M', '.', 'M' },
+    },
+    {
+        { 'M', '.', 'M' },
+        { '.', 'A', '.' },
+        { 'S', '.', 'S' },
+    },
+};
+
 int countXMAS(vector< vector<char> > &map, int row, int col, vector< vector<char> > &shadowMap) {
     int rows = map.size();
     int cols = map[0].size();
+
+    if (map[row][col] != 'A') return 0;
     
-    int count = 0;    
-    for (WordDirection dir : directions) {
+    for (vector<vector<char>> matrix : MAS_MATRICES) {
         bool shouldCount = true;
-        for (int i = 0; i < word.size(); i++) {
-            char c = word[i];
-            int ox = (i - 1) * dir.dx + col;
-            int oy = (i - 1) * dir.dy + row;
-            if (ox < 0 || ox >= cols || oy < 0 || oy >= rows) {
-                shouldCount = false;
-                // cout << "Out of bounds" << endl;
-                break;
-            }
-            if (map[oy][ox] != c) {
-                shouldCount = false;
-                // cout << "Wrong char, " << c << " got " << map[oy][ox] << endl;
-                break;
+        for (int _row = 0; _row < 3; _row++) {
+            vector<char> matrixRow = matrix[_row];
+            for (int _col = 0; _col < 3; _col++) {
+                char c = matrixRow[_col];
+                
+                int mRow = row - 1 + _row;
+                int mCol = col - 1 + _col;
+                if (mRow < 0 || mRow >= rows || mCol < 0 || mCol >= cols) {
+                    return 0;
+                }
+                if (c == '.') {
+                    continue;
+                }
+                if (map[mRow][mCol] != c) {
+                    shouldCount = false;
+                    break;
+                }
             }
         }
         if (shouldCount) {
-
-            for (int i = 0; i < word.size(); i++) {
-                int ox = (i - 1) * dir.dx + col;
-                int oy = (i - 1) * dir.dy + row;
-                shadowMap[oy][ox] = map[oy][ox];
-            }
-
-            // cout << "Added" << endl;
-            count += 1;
+            return 1;
         }
     }
-    
-    return count;
+    return 0;
+
+    // for (WordDirection dir : directions) {
+    //     bool shouldCount = true;
+    //     for (int i = 0; i < word.size(); i++) {
+    //         char c = word[i];
+    //         int ox = (i - 1) * dir.dx + col;
+    //         int oy = (i - 1) * dir.dy + row;
+    //         if (ox < 0 || ox >= cols || oy < 0 || oy >= rows) {
+    //             shouldCount = false;
+    //             // cout << "Out of bounds" << endl;
+    //             break;
+    //         }
+    //         if (map[oy][ox] != c) {
+    //             shouldCount = false;
+    //             // cout << "Wrong char, " << c << " got " << map[oy][ox] << endl;
+    //             break;
+    //         }
+    //     }
+    //     if (shouldCount) {
+    //
+    //         for (int i = 0; i < word.size(); i++) {
+    //             int ox = (i - 1) * dir.dx + col;
+    //             int oy = (i - 1) * dir.dy + row;
+    //             shadowMap[oy][ox] = map[oy][ox];
+    //         }
+    //
+    //         // cout << "Added" << endl;
+    //         count += 1;
+    //     }
+    // }
 }
 
 void printMap(vector< vector<char>> map) {
